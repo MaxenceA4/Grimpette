1323d1800860204d4da52dcfeb28a5e916665ed4
MaxenceA <76045198+MaxenceA4@users.noreply.github.com>
2023-07-20 10:07:14 +0200

    Légère description reprise du 20/07

--- index.php
+++ index.php
@@ -1,1 +1,34 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <title>Grimpe!</title>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/stylesheet.css">
+</head>
+<body>
+    <header>
+        <h1>Grimpe!</h1>
+    </header>
+<div class="wrapper">
+<nav>
+    <ul>
+        <li><a class="active" href="index.php">Accueil</a></li>
+        <li><a href="index.php?page=contact">Contact</a></li>
+        <li><a href="index.php?page=inscription">Inscription</a></li>
+        <li><a href="index.php?page=connexion">Connexion</a></li>
+    </ul>
+</nav>
+<main>
+    <h2>Accueil</h2>
+    <h3>Présentation</h3>
+    <p> Bienvenue sur Grimpe!, le site de défis entre potes !</p>
+    <p> Vous pouvez rejoindre la communauté d'une salle en particulier !</p>
+    <p> Vous pouvez vous inscrire pour créer des défis, ou vous connecter pour les relever !</p>
+    <p> Vous pouvez aussi nous contacter pour nous faire part de vos remarques !</p>
+</main>
+<footer>
+    <p> &copy; 2023 - Grimpe!</p>
+</footer>
+</div>
+</body>
+</html>
